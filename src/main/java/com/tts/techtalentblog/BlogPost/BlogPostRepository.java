e20e73294b5f67ab3b6be9e5978cf2702f8e937c
jacob-huss <72151699+jacob-huss@users.noreply.github.com>
2020-10-21 11:44:08 -0500

    BlogProjectPartialStage2

--- src/main/java/com/tts/techtalentblog/BlogPost/BlogPostRepository.java
+++ src/main/java/com/tts/techtalentblog/BlogPost/BlogPostRepository.java
@@ -3,5 +3,5 @@ package com.tts.techtalentblog.BlogPost;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
-    
+
 }
